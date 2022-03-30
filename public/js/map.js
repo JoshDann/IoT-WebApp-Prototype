14d40891564682c0273047063d53470c46978e65
JoshDann <j.dann@rgu.ac.uk>
2022-03-30 13:52:39 +0100

    added geocoder back to map data page

--- public/js/map.js
+++ public/js/map.js
@@ -2,30 +2,36 @@ mapboxgl.accessToken = 'pk.eyJ1IjoiamRhbm4xIiwiYSI6ImNsMGw5cmRzeDBmOWEzanBla3o1Y
 const map = new mapboxgl.Map({
     container: 'map',
     style: 'mapbox://styles/jdann1/cl0v7nlgv000h15t46mhhpw2n',
-    center: [0,0],
+    center: [0, 0],
     zoom: 1
 });
 
 map.on('load', () => {
-
 });
 
-$('.form_button_submit').click(function(){
+$('.form_button_submit').click(function () {
     let formdata = $('#form_map').serializeArray();
     //console.log(formdata);
 });
 
 
 
-$('#form_map').submit(function(e){
+$('#form_map').submit(function (e) {
     e.preventDefault();
+
+    //removes layer and source if search has already been made and displaying data on map
+    //error occurs if not doing this
+    let source = map.getSource('node-locations-source');
+    if (source != undefined) {
+        map.removeLayer('node-locations-layer');
+        map.removeSource('node-locations-source');
+    }
+
     let formdata = $('#form_map').serializeArray();
-    //console.log(formdata);
 
     let queryString = "index.js/data?";
 
-    $.get("/data", function(data){
-        console.log(data);
+    $.get("/data", function (data) {
 
         map.addSource('node-locations-source', {
             type: 'geojson',
@@ -33,43 +39,33 @@ $('#form_map').submit(function(e){
         });
 
         map.addLayer({
-            'id': 'node-locations-layer',
-            'type': 'circle',
-            'source': 'node-locations-source'
+            id: 'node-locations-layer',
+            type: 'circle',
+            source: 'node-locations-source',
+            paint: {
+                'circle-radius': {
+                    'base': 1.75,
+                    'stops': [
+                        [0, 10],
+                        [12, 5],
+                        [20, 10]
+                    ]
+                }
+            }
         })
     });
 });
 
 //Geocoder stuff, don't know if we need for the regular map page
 
-// const geocoder = new MapboxGeocoder({
-//     accessToken: mapboxgl.accessToken,
-//     mapboxgl: mapboxgl,
-//     flyTo: true,
-//     autoComplete: true,
-//     fuzzyMatch: true,
-//     marker: false
-// });
+const geocoder = new MapboxGeocoder({
+    accessToken: mapboxgl.accessToken,
+    mapboxgl: mapboxgl,
+    flyTo: true,
+    autoComplete: true,
+    fuzzyMatch: true,
+});
 
-// let lngLat = {lng: 0, lat: 0};
-
-// geocoder.on('result', search => {
-//     const marker = new mapboxgl.Marker({
-//         draggable: true
-//     })
-
-//     marker.setLngLat(search.result.center);
-//     lngLat.lng = search.result.center[0];
-//     lngLat.lat = search.result.center[1];
-//     console.log(lngLat);
-
-//     marker.addTo(map);
-//     marker.on('dragend', function(search){
-//         lngLat = search.target.getLngLat();
-//         console.log(lngLat);
-//     })
-// })
-
-// map.addControl(geocoder);
+map.addControl(geocoder);
 
 
