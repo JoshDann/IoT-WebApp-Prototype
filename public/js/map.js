15674ea922bbe2071e59ad99ff021e439977d885
JoshDann <j.dann@rgu.ac.uk>
2022-04-03 18:32:45 +0100

    small edits

--- public/js/map.js
+++ public/js/map.js
@@ -2,7 +2,7 @@ mapboxgl.accessToken = 'pk.eyJ1IjoiamRhbm4xIiwiYSI6ImNsMGw5cmRzeDBmOWEzanBla3o1Y
 const map = new mapboxgl.Map({
     container: 'map',
     style: 'mapbox://styles/jdann1/cl0v7nlgv000h15t46mhhpw2n',
-    center: [0,0],
+    center: [0, 0],
     zoom: 1
 });
 
@@ -28,7 +28,7 @@ map.on('load', () => {
 /**
  * Get and add all points for readings that match form criteria (for now, just all data)
  */
-$('#form_button_submit').click( function(ev) {
+$('#form_button_submit').click(function (ev) {
     // let formData = $('#form_map').serializeArray()
     // let queryString = "index.js/data?";
     getReadings()
@@ -39,43 +39,47 @@ $('#form_button_submit').click( function(ev) {
  * Add params later on
  */
 function getReadings() {
-    $.getJSON("/data", function(data) {
+    $.getJSON("/data", function (data) {
         // prints all readings returned by /data
-        data.forEach( reading => { console.table(reading) } )
+        data.forEach(reading => { console.table(reading) })
+
+        //adds source to draw the data for drawing the circles
+        map.addSource('node-locations-source', {
+            type: 'geojson',
+            data: data
+        })
+
+        //adds the circles and styles them
+        map.addLayer({
+            id: 'node-locations-layer',
+            type: 'circle',
+            source: 'node-locations-source',
+            paint: {
+                'circle-opacity': 0.5,
+                'circle-radius':
+                    [
+                        //increases circle size as zoom in
+                        //first number is zoom level, 0=zoomed out 22=max zoom in
+                        //second number is pixel size of the circles
+                        'interpolate', ['linear'], ['zoom'],
+                        5, 2,
+                        10, 8,
+                        15, 15,
+                        20, 40
+                    ],
+                'circle-color': {
+                    'property': 'reading',
+                    'stops': [
+                        [0, 'green'],
+                        [50, 'orange'],
+                        [100, 'red']
+                    ]
+                }
+            }
+        })
     })
 }
 
-// map.addSource('node-locations-source', {
-//     type: 'geojson',
-//     data: data
-// })
 
-// adds the circles and styles them
-//     map.addLayer({
-//         id: 'node-locations-layer',
-//         type: 'circle',
-//         source: 'node-locations-source',
-//         paint: {
-//             'circle-opacity' : 0.5,
-//             'circle-radius':
-//                 [
-//                     //increases circle size as zoom in
-//                     //first number is zoom level, 0=zoomed out 22=max zoom in
-//                     //second number is pixel size of the circles
-//                     'interpolate', ['linear'], ['zoom'],
-//                     5, 2,
-//                     10, 8,
-//                     15, 15,
-//                     20, 40
-//                 ],
-//             'circle-color': {
-//                 'property': 'reading',
-//                 'stops': [
-//                     [0, 'green'],
-//                     [50, 'orange'],
-//                     [100, 'red']                    
-//                 ]
-//             }
-//         }
-//     })
-// })
+
+
